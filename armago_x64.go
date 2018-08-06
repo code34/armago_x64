9557fe4aa4787dcab847b04f8f007b6cdd0dd531
code34 <nicolas_boiteux@yahoo.fr>
2018-08-06 23:33:40 +0200

    add rvextensionversion fix rvextensionargs fix outputsize

--- armago_x64.go
+++ armago_x64.go
@@ -12,27 +12,26 @@ import (
 	"unsafe"
 )
 
+//export RVExtensionVersion
+func RVExtensionVersion(output *C.char, outputsize C.size_t) {
+	result := C.CString("Version 1.0")
+	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), outputsize - 1)
+}
+
 //export RVExtensionArgs
 func RVExtensionArgs(output *C.char, outputsize C.size_t, input *C.char, argv **C.char, argc C.int) {
 	var offset = unsafe.Sizeof(uintptr(0))
 	var out []string
-	limit := *(*int)(unsafe.Pointer(&argc))
-	if limit < 1000 {
-		for index := 0; index < limit; index++ {
-			out = append(out, C.GoString(*argv))
-			argv = (**C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(argv)) + offset))
-		}
+	for index := C.int(0); index < argc; index++ {
+		out = append(out, C.GoString(*argv))
+		argv = (**C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(argv)) + offset))
 	}
-	temp := fmt.Sprintf("Hello %s %d %s!", C.GoString(input), limit,  out)
+	temp := fmt.Sprintf("Function: %s nb params: %d params: %s!", C.GoString(input), argc,  out)
 
 	// Return a result to Arma
 	result := C.CString(temp)
 	defer C.free(unsafe.Pointer(result))
-	var size = C.strlen(result) + 1
-	if size > outputsize {
-		size = outputsize
-	}
-	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), size)
+	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), outputsize - 1)
 }
 
 //export RVExtension
@@ -41,11 +40,7 @@ func RVExtension(output *C.char, outputsize C.size_t, input *C.char) {
 	// Return a result to Arma
 	result := C.CString(temp)
 	defer C.free(unsafe.Pointer(result))
-	var size = C.strlen(result) + 1
-	if size > outputsize {
-		size = outputsize
-	}
-	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), size)
+	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), outputsize - 1)
 }
 
 func main() {}
