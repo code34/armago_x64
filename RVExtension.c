ba32ab17e0959197f4451e4dd5e42149d7bff3ab
Nicolas Boiteux <nicolas_boiteux@yahoo.fr>
2019-06-30 11:46:10 +0200

    add RVExtensionversion & RVExtensionargs

--- RVExtension.c
+++ RVExtension.c
@@ -1,9 +1,19 @@
 #include <stdlib.h>
 
 extern void goRVExtension(char *output, size_t outputSize, char *input);
+extern void goRVExtensionVersion(char *output, size_t outputSize);
+extern void goRVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc);
 
 __declspec(dllexport) void __stdcall _RVExtension(char *output, size_t outputSize, char *input) {
   goRVExtension(output, outputSize, input);
+}
+
+__declspec(dllexport) void __stdcall _RVExtensionVersion(char *output, size_t outputSize) {
+  goRVExtensionVersion(output, outputSize);
+}
+
+__declspec(dllexport) void __stdcall _RVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc) {
+  goRVExtensionArgs(output, outputSize, input, argv, argc);
 }
 
 // do this for all the other exported functions
--- armago.go
+++ armago.go
@@ -1,14 +1,47 @@
 package main
 
 /*
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 */
 import "C"
 
-import "unsafe"
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+//export goRVExtensionVersion
+func goRVExtensionVersion(output *C.char, outputsize C.size_t) {
+	result := C.CString("Version 1.0")
+	defer C.free(unsafe.Pointer(result))
+	var size = C.strlen(result) + 1
+	if size > outputsize {
+		size = outputsize
+	}
+	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), size)
+}
+
+//export goRVExtensionArgs
+func goRVExtensionArgs(output *C.char, outputsize C.size_t, input *C.char, argv **C.char, argc C.int) {
+	var offset = unsafe.Sizeof(uintptr(0))
+	var out []string
+	for index := C.int(0); index < argc; index++ {
+		out = append(out, C.GoString(*argv))
+		argv = (**C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(argv)) + offset))
+	}
+	temp := fmt.Sprintf("Function: %s nb params: %d params: %s!", C.GoString(input), argc,  out)
+
+	// Return a result to Arma
+	result := C.CString(temp)
+	defer C.free(unsafe.Pointer(result))
+	var size = C.strlen(result) + 1
+	if size > outputsize {
+		size = outputsize
+	}
+	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), size)
+}
 
 //export goRVExtension
 func goRVExtension(output *C.char, outputsize C.size_t, input *C.char) {
