c2e92a3d00d277d18e93a3867f0e1c8792bb2304
Eathox <Rendier3636@gmail.com>
2020-12-06 12:10:42 +0100

    Add extensionCallBack support

--- RVExtension.c
+++ RVExtension.c
@@ -1,32 +1,43 @@
 #include <stdlib.h>
 
+#include "extensionCallback.h"
+
 extern void goRVExtension(char *output, size_t outputSize, char *input);
 extern void goRVExtensionVersion(char *output, size_t outputSize);
 extern void goRVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc);
+extern void goRVExtensionRegisterCallback(extensionCallback fnc);
 
 #ifdef WIN64
 void RVExtension(char *output, size_t outputSize, char *input) {
 	goRVExtension(output, outputSize, input);
 }
- 
+
 void RVExtensionVersion(char *output, size_t outputSize) {
 	goRVExtensionVersion(output, outputSize);
 }
- 
+
 void RVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc) {
 	goRVExtensionArgs(output, outputSize, input, argv, argc);
 }
+
+void RVExtensionRegisterCallback(extensionCallback fnc) {
+    goRVExtensionRegisterCallback(fnc);
+}
 #else
 __declspec(dllexport) void __stdcall _RVExtension(char *output, size_t outputSize, char *input) {
-  goRVExtension(output, outputSize, input);
+	goRVExtension(output, outputSize, input);
 }
 
 __declspec(dllexport) void __stdcall _RVExtensionVersion(char *output, size_t outputSize) {
-  goRVExtensionVersion(output, outputSize);
+	goRVExtensionVersion(output, outputSize);
 }
 
 __declspec(dllexport) void __stdcall _RVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc) {
-  goRVExtensionArgs(output, outputSize, input, argv, argc);
+	goRVExtensionArgs(output, outputSize, input, argv, argc);
+}
+
+__declspec(dllexport) void __stdcall _RVExtensionRegisterCallback(extensionCallback fnc) {
+    goRVExtensionRegisterCallback(fnc);
 }
 #endif
 // do this for all the other exported functions
--- armago.go
+++ armago.go
@@ -4,13 +4,18 @@ package main
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+#include "extensionCallback.h"
 */
 import "C"
 
 import (
 	"fmt"
+	"time"
 	"unsafe"
 )
+
+var extensionCallbackFnc C.extensionCallback
 
 //export goRVExtensionVersion
 func goRVExtensionVersion(output *C.char, outputsize C.size_t) {
@@ -31,7 +36,7 @@ func goRVExtensionArgs(output *C.char, outputsize C.size_t, input *C.char, argv 
 		out = append(out, C.GoString(*argv))
 		argv = (**C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(argv)) + offset))
 	}
-	temp := fmt.Sprintf("Function: %s nb params: %d params: %s!", C.GoString(input), argc,  out)
+	temp := fmt.Sprintf("Function: %s nb params: %d params: %s!", C.GoString(input), argc, out)
 
 	// Return a result to Arma
 	result := C.CString(temp)
@@ -43,9 +48,26 @@ func goRVExtensionArgs(output *C.char, outputsize C.size_t, input *C.char, argv 
 	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), size)
 }
 
+func callBackExample() {
+	name := C.CString("armago")
+	defer C.free(unsafe.Pointer(name))
+	function := C.CString("funcToExecute")
+	defer C.free(unsafe.Pointer(function))
+	// Make a callback to Arma
+	for i := 0; i < 3; i++ {
+		time.Sleep(2 * time.Second)
+		param := C.CString(fmt.Sprintf("Loop: %d", i))
+		defer C.free(unsafe.Pointer(param))
+		C.runExtensionCallback(extensionCallbackFnc, name, function, param)
+	}
+}
+
 //export goRVExtension
 func goRVExtension(output *C.char, outputsize C.size_t, input *C.char) {
 	temp := fmt.Sprintf("Hello %s!", C.GoString(input))
+	if extensionCallbackFnc != nil {
+		go callBackExample()
+	}
 	// Return a result to Arma
 	result := C.CString(temp)
 	defer C.free(unsafe.Pointer(result))
@@ -54,6 +76,11 @@ func goRVExtension(output *C.char, outputsize C.size_t, input *C.char) {
 		size = outputsize
 	}
 	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), size)
+}
+
+//export goRVExtensionRegisterCallback
+func goRVExtensionRegisterCallback(fnc C.extensionCallback) {
+	extensionCallbackFnc = fnc
 }
 
 func main() {}
